aaebbcb3cbba06092669da57815af472d0e70581
AdrianeDriane <dilaoaparre@gmail.com>
2026-03-19 21:08:07 +0800

    client: fix router import and restore prettier lint deps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import router from "./router";
 import { store } from "./store";
 
 const queryClient = new QueryClient();
@@ -15,3 +15,4 @@ export default function App() {
     </Provider>
   );
 }
+
